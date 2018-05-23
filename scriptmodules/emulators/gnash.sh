aa59046ce08749921334d9614560179c7803b5ed
Super <22881403+SuperCatFooz@users.noreply.github.com>
2018-05-23 18:06:06 -0500

    Added Gnash - SWF player for Flash games
    Added Gnash script.  I couldn't quite figure out the proper way to write the `addEmulator` command while adhering to the Script Style Guide used by RetroPie, so feel free to rewrite that portion of the script.
    
    This isn't really an emulator per-se (calling it one would be like calling VLC an MP4 emulator), but it doesn't quite fit into ports either since you have to supply your own games.
    
    Terminology aside, I've tested this on my Pi 3 non-overclocked and it runs older games pretty well all things considered; however it can't properly run games made past SWF 10 (which rougly covers everything made after 2009 or so), and performace is pretty spotty on more intensive games such as Mario 63.

---
+++ scriptmodules/emulators/gnash.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+# 
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+# 
+# See the LICENSE.md file at the top-level directory of this distribution and 
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="gnash"
+rp_module_desc="gnash - Adobe Flash SWF Player"
+rp_module_help="ROM Extensions: .swf\n\nCopy your Flash games to $romdir/flash\n\nYou need to set video mode to CEA-1 in Runcommand."
+rp_module_licence="GPL3 http://git.savannah.gnu.org/cgit/gnash.git/tree/COPYING"
+rp_module_section="exp"
+rp_module_flags="!mali !kms"
+
+function depends_gnash() {
+	getDepends xinit
+}
+
+function install_bin_gnash() {
+	aptInstall gnash
+}
+
+function configure_gnash() {
+	mkRomDir "flash"
+	addEmulator 1 "$md_id" "flash" "sudo xinit -e 'gnash %ROM%  -j 640 -k 480 --hide-menubar' -- :0"
+	addSystem "flash"
+}
